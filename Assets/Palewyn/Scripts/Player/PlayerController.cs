86cb9d250bc142b968da827e14e59ed71b43ba8a
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-09-28 12:11:14 +1000

    yeet

---
+++ Assets/Palewyn/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private CharacterController controller;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        Move(inputH, inputV);
+    }
+    void Move(float inputH, float inputV)
+    {
+        Vector3 moveDirection = new Vector3(inputH, 0f, inputV);
+        controller.Move(moveDirection * speed * Time.deltaTime);
+        anim.SetFloat("MoveDirection", moveDirection.z);
+        anim.SetBool("IsWalking", moveDirection.magnitude > 0);
+    }
+}
--- Assets/Palewyn/Scripts/Tower/Placement/TowerPlacement.cs
+++ Assets/Palewyn/Scripts/Tower/Placement/TowerPlacement.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using TowerDefense.Towers.Placement;
 using Core.Utilities;
 using UnityEngine;
-
-
-public class TowerPlacement : MonoBehaviour
+namespace Palewyn
 {
-    public LayerMask hitLayers;
-
-    private void OnDrawGizmos()
+    public class TowerPlacement : MonoBehaviour
     {
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(camRay.origin, camRay.origin + camRay.direction * 1000f);
-    }
+        public LayerMask hitLayers;
 
-    private void FixedUpdate()
-    {
-        if (Input.GetMouseButtonDown(0))
+        private void OnDrawGizmos()
         {
             Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(camRay, out hit, 1000f, hitLayers))
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(camRay.origin, camRay.origin + camRay.direction * 1000f);
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetMouseButtonDown(0))
             {
-                IPlacementArea placement = hit.collider.GetComponent<IPlacementArea>();
-                if (placement != null)
+                Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(camRay, out hit, 1000f, hitLayers))
                 {
-                    transform.position = placement.Snap(hit.point, new IntVector2(1, 1));
+                    IPlacementArea placement = hit.collider.GetComponent<IPlacementArea>();
+                    if (placement != null)
+                    {
+                        transform.position = placement.Snap(hit.point, new IntVector2(1, 1));
+                    }
                 }
             }
         }
